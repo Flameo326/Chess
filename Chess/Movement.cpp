e04b96c8eb4c6861bc55976c99252bf4e7f37415
lmckamey <lucas.mckamey1@gmail.com>
2019-08-22 09:49:47 -0600

    Moved the get all movement logic into the movment class because it makes more sense

--- Chess/Movement.cpp
+++ Chess/Movement.cpp
@@ -436,6 +436,230 @@ namespace Chess {
 		return mvmt;
 	}
 
+	MapOfCoords Movement::getAllPawnMovements(bool isWhite) {
+		//if isWhite is true get all the white pawns, else get all the black pawns
+		std::bitset<64> pawns = isWhite ? board.getPawns() & board.getWhiteTeam() : board.getPawns() & board.getBlackTeam();
+		MapOfCoords movements;
+
+		//find all the indivdual pawns
+		for (size_t j = 0; j < 64; j++)
+		{
+			//if this index is active this is a piece
+			if (pawns[j]) {
+				//make this piece into an idvidual bitset
+				std::bitset<64> piece;
+				piece.set(j);
+
+				//get the Coord of the piece
+				Coord pieceCoord = board.Bit64ToXAndY(piece).at(0);
+
+				//find all the movement bits of the piece
+				std::bitset<64> movementBits = getPawnMvmt(piece, isWhite);
+				//check if there are valid moves
+				if (movementBits.count() > 0) {
+					//check if there are valid moves
+					std::vector<Coord> movementCoordinates = board.Bit64ToXAndY(movementBits);
+					//add the movements to the movement map
+					movements[pieceCoord] = movementCoordinates;
+				}
+			}
+		}
+		return movements;
+	}
+
+	MapOfCoords Movement::getAllRookMovements(bool isWhite) {
+		//if isWhite is true get all the white rooks, else get all the black rooks
+		std::bitset<64> rooks = isWhite ? board.getRooks() & board.getWhiteTeam() : board.getRooks() & board.getBlackTeam();
+		MapOfCoords movements;
+
+		//find all the indivdual rooks
+		for (size_t j = 0; j < 64; j++)
+		{
+			//if this index is active this is a piece
+			if (rooks[j]) {
+				//make this piece into an idvidual bitset
+				std::bitset<64> piece;
+				piece.set(j);
+
+				//get the Coord of the piece
+				std::vector<Coord> pieceCoord = board.Bit64ToXAndY(piece);
+
+				//find all the movement bits of the piece
+				std::bitset<64> movementBits = getRookMvmt(piece, isWhite);
+				//check if there are valid moves
+				if (movementBits.count() > 0) {
+					//check if there are valid moves
+					std::vector<Coord> movementCoordinates = board.Bit64ToXAndY(movementBits);
+
+					//add the movements to the movement map
+					movements[pieceCoord.at(0)] = movementCoordinates;
+				}
+			}
+		}
+		return movements;
+	}
+
+	MapOfCoords Movement::getAllBishopMovements(bool isWhite) {
+		//if isWhite is true get all the white bishops, else get all the black bishops
+		std::bitset<64> bishops = isWhite ? board.getBishops() & board.getWhiteTeam() : board.getBishops() & board.getBlackTeam();
+		MapOfCoords movements;
+
+		//find all the indivdual bishops
+		for (size_t j = 0; j < 64; j++)
+		{
+			//if this index is active this is a piece
+			if (bishops[j]) {
+				//make this piece into an idvidual bitset
+				std::bitset<64> piece;
+				piece.set(j);
+
+				//get the Coord of the piece
+				std::vector<Coord> pieceCoord = board.Bit64ToXAndY(piece);
+
+				//find all the movement bits of the piece
+				std::bitset<64> movementBits = getBishopMvmt(piece, isWhite);
+				//check if there are valid moves
+				if (movementBits.count() > 0) {
+					//check if there are valid moves
+					std::vector<Coord> movementCoordinates = board.Bit64ToXAndY(movementBits);
+
+					//add the movements to the movement map
+					movements[pieceCoord.at(0)] = movementCoordinates;
+				}
+			}
+		}
+		return movements;
+	}
+
+	MapOfCoords Movement::getAllKnightMovements(bool isWhite) {
+		//if isWhite is true get all the white Kinghts, else get all the black Knights
+		std::bitset<64> knights = isWhite ? board.getKnights() & board.getWhiteTeam() : board.getKnights() & board.getBlackTeam();
+		MapOfCoords movements;
+
+		//find all the indivdual Knights
+		for (size_t j = 0; j < 64; j++)
+		{
+			//if this index is active this is a piece
+			if (knights[j]) {
+				//make this piece into an idvidual bitset
+				std::bitset<64> piece;
+				piece.set(j);
+
+				//get the Coord of the piece
+				std::vector<Coord> pieceCoord = board.Bit64ToXAndY(piece);
+
+				//find all the movement bits of the piece
+				std::bitset<64> movementBits = getKnightMvmt(piece, isWhite);
+				//check if there are valid moves
+				if (movementBits.count() > 0) {
+					//check if there are valid moves
+					std::vector<Coord> movementCoordinates = board.Bit64ToXAndY(movementBits);
+
+					//add the movements to the movement map
+					movements[pieceCoord.at(0)] = movementCoordinates;
+				}
+			}
+		}
+		return movements;
+	}
+
+	MapOfCoords Movement::getAllQueenMovements(bool isWhite) {
+		//Check which team is active and grab all of that teams queens
+		std::bitset<64> queens = isWhite ? board.getQueens() & board.getWhiteTeam() : board.getQueens() & board.getBlackTeam();
+		MapOfCoords movements;
+
+		//find all the "pieces in the bitset
+		for (size_t j = 0; j < 64; j++)
+		{
+			//if this index is a 1 then it is a piece
+			if (queens[j]) {
+				std::bitset<64> piece;
+				//set the new new bitset equal the the indivual "piece" location
+				piece.set(j);
+
+				//make a vector of Coords from the piece bitset
+				std::vector<Coord> pieceCoord = board.Bit64ToXAndY(piece);
+
+				//get all the potential moves bitset
+				std::bitset<64> movementBits = getQueenMvmt(piece, isWhite);
+				//if there is a valid move
+				if (movementBits.count() > 0) {
+					//get the movement coordinates
+					std::vector<Coord> movementCoordinates = board.Bit64ToXAndY(movementBits);
+
+					//add the movements coordinates to the map with the key of the piece
+					movements[pieceCoord.at(0)] = movementCoordinates;
+				}
+			}
+		}
+		return movements;
+	}
+
+	MapOfCoords Movement::getAllKingMovements(bool isWhite) {
+		//Chek which team is active and grab all of that teams kings
+		std::bitset<64> kings = isWhite ? board.getQueens() & board.getWhiteTeam() : board.getQueens() & board.getBlackTeam();
+		MapOfCoords map;
+
+		//find all the "pieces" in the bitset
+		for (size_t j = 0; j < 64; j++)
+		{
+			//if one of the elements is on(is = 1 or true)
+			if (kings[j]) {
+				std::bitset<64> piece;
+				//set the new new bitset equal the the indivual "piece" location
+				piece.set(j);
+
+				//make a vector of Coords from the piece bitset
+				std::vector<Coord> pieceCoord = board.Bit64ToXAndY(piece);
+
+				//get all the potential moves bitset
+				std::bitset<64> movementBits = getKingMvmt(piece, isWhite);
+				//if there is a valid move
+				if (movementBits.count() > 0) {
+					//get the movement coordinates
+					std::vector<Coord> movementCoordinates = board.Bit64ToXAndY(movementBits);
+					//add the movements coordinates to the map with the key of the piece
+					map[pieceCoord.at(0)] = movementCoordinates;
+				}
+			}
+		}
+		return map;
+	}
+
+	MapOfCoords Movement::getAllAvailableMoves(bool isWhite)
+	{
+		MapOfCoords allPossibleMoves;
+
+		//Get the potential Pawn movments
+		MapOfCoords pawnMovements = getAllPawnMovements(isWhite);
+		//Get the potential Bishop movements
+		MapOfCoords bishopMovments = getAllBishopMovements(isWhite);
+		//Get the potential Rook movments
+		MapOfCoords rookMovments = getAllRookMovements(isWhite);
+		//Get the potential Knight movments
+		MapOfCoords knightMovements = getAllKnightMovements(isWhite);
+		//Get the potential Queen movments
+		MapOfCoords queenMovements = getAllQueenMovements(isWhite);
+		//Get the potential King movements
+		MapOfCoords kingMovements = getAllQueenMovements(isWhite);
+
+		//add the pawn movements to the map
+		allPossibleMoves.insert(pawnMovements.begin(), pawnMovements.end());
+		//add the bishop movements to the map
+		allPossibleMoves.insert(bishopMovments.begin(), bishopMovments.end());
+		//add the rook movements to the map
+		allPossibleMoves.insert(rookMovments.begin(), rookMovments.end());
+		//add the knight movments to the map
+		allPossibleMoves.insert(knightMovements.begin(), knightMovements.end());
+		//add the queen movments to the map
+		allPossibleMoves.insert(queenMovements.begin(), queenMovements.end());
+		//add the king movements to the map
+		allPossibleMoves.insert(kingMovements.begin(), kingMovements.end());
+
+		//return a unordered map of all the possible moves
+		return allPossibleMoves;
+	}
+
 	// Checks position of Pawns,
 	// Then Checks History
 	std::bitset<64> Movement::checkForEnPassant(std::bitset<64> &king, std::bitset<64> &pawnPos, bool isWhite) {
